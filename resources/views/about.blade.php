4f38d0ce71f4c31de06b1f83809e2ed580d7e8b3
RifkyPuji2 <rifky.pras0801@gmail.com>
2022-02-12 12:19:11 +0700

    update laravel 2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Rifky Puji | About</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
     <h1>Halaman About</h1>
-    <h3> <?php echo $nama; ?></h3>
-    <p> <?php echo $email; ?></p>
-    <p>Follow IG @Rifky.P23</p>
-    <img src="images/<?php echo $gambar; ?>" alt="<?php echo $nama; ?>" width="200px">
-</body>
-</html>
+    <h4> {{ $nama }} </h4>
+    <p> {{ $email }} </p>
+    <img src="images/{{ $gambar }}" alt="{{ $nama }}" width="200px">
+@endsection
