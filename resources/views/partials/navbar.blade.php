cce5bdec1b3bd2605f25b7fc7420f6e46d40972e
RifkyPuji2 <rifky.pras0801@gmail.com>
2022-03-09 18:48:54 +0700

    Modul 7

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,30 +4,28 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <ul class="navbar-nav">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link {{ ( $title === 'Beranda') ? 'active' : '' }}" aria-current="page" href="/">Beranda</a>
+          <a class="nav-link {{ ( $title === 'Home') ? 'active': '' }}" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ ( $title === 'About') ? 'active' : '' }}" href="/about">About</a>
+          <a class="nav-link {{ ( $title === 'About') ? 'active': '' }}" href="/about">About</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ ( $title === 'Gallery') ? 'active' : '' }}" href="/gallery">gallery</a>
+          <a class="nav-link {{ ( $title === 'Gallery') ? 'active': '' }}" href="/gallery">Gallery</a>
+        <li class="nav-item">
+          <a class="nav-link {{ ( $title === 'Contacts') ? 'active': '' }}" href=" {{ route('contacts.create')}}">Contact Us</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="{{route ('login')}}">Login</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="/contacts">Contacts</a>
+          <a class="nav-link" href="{{route ('register')}}">Register</a>
         </li>
-
-    </ul>
-    <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('login')}}">Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('register')}}">Register</a>
-            </li>
-          </ul>
+      </ul>
     </div>
   </div>
 </nav>
